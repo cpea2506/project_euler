8f968d1261f3851c655cb316c4527de7a7bb737d
cpea2506 <vienpro2506@gmail.com>
2022-02-23 02:03:36 +0700

    problem 3: shorten solution

--- src/bin/prob3.rs
+++ src/bin/prob3.rs
@@ -1,9 +1,6 @@
 // Largest prime factor
 
-fn largest_prime_factor(num: u64) -> u64 {
-    let mut num = num;
-    let mut max_prime = Default::default();
-
+fn largest_prime_factor(mut num: u64) -> u64 {
     while num % 2 == 0 {
         num >>= 1;
     }
@@ -16,30 +13,23 @@ fn largest_prime_factor(num: u64) -> u64 {
     let mut i = 5;
     while i * i < num {
         while num % i == 0 {
-            max_prime = i;
             num /= i;
         }
 
-        // difference of two prime factors must
-        // be at least 2 (except 2 and 3)
         while num % (i + 2) == 0 {
-            max_prime = i + 2;
             num /= i + 2;
         }
 
         i += 6;
     }
 
-    // if the last prime factor is not 2 and 3. Take it!
-    if num > 4 {
-        max_prime = num;
-    }
-
-    max_prime
+    num
 }
 
 pj_euler::solution!("Largest prime factor", largest_prime_factor(600851475143));
 
-pj_euler::test!({
-    largest_prime_factor_of_13195, largest_prime_factor(13195), 29
-});
+pj_euler::test!(
+    {largest_prime_factor_of_13195, largest_prime_factor(13195), 29},
+    {largest_prime_factor_of_333, largest_prime_factor(333), 37},
+    {largest_prime_factor_of_331, largest_prime_factor(331), 331}
+);
