1e590442e367224e2946e4439b7f810d0c9a9e10
Do Vien <vienpro2506@gmail.com>
2022-04-05 15:46:53 +0700

    prob1: optimize solution

--- src/bin/prob1.rs
+++ src/bin/prob1.rs
@@ -1,12 +1,21 @@
 // Multiples of 3 or 5
 
-fn sum(bound: u16) -> u16 {
-    (3..bound).filter(|n| n % 3 == 0 || n % 5 == 0).sum()
+fn sum_divisible_by(target: u32, n: u32) -> u32 {
+    let p = (target - 1) / n;
+
+    n * p * (p + 1) / 2
+}
+
+fn sum(target: u32) -> u32 {
+    sum_divisible_by(target, 3) + sum_divisible_by(target, 5) - sum_divisible_by(target, 15)
 }
 
 pj_euler::solution!("Multiple of 3 or 5", sum(1000));
 
 pj_euler::test!(
+    {sum_divisible_by_3_with_target_10, sum_divisible_by(10, 3), 18},
+    {sum_divisible_by_5_with_target_10, sum_divisible_by(10, 5), 5},
+    {sum_divisible_by_15_with_target_10, sum_divisible_by(10, 15), 0},
     {sum_below_ten, sum(10), 23},
     {sum_above_ten, sum(20), 78}
 );
