d9460c5add41550565fbb7a09d4a4c03bda4019c
Do Vien <vienpro2506@gmail.com>
2022-04-07 09:03:12 +0700

    prob7: create Prime trait for u32

--- src/bin/prob7.rs
+++ src/bin/prob7.rs
@@ -1,42 +1,50 @@
 // 10001st prime
 
-fn is_prime(n: u32) -> bool {
-    if n <= 1 {
-        return false;
-    }
+trait Prime {
+    fn is_prime(&self) -> bool;
+}
 
-    if n < 4 {
-        return true;
-    }
+impl Prime for u32 {
+    fn is_prime(&self) -> bool {
+        let n = *self;
 
-    if n % 2 == 0 {
-        return false;
-    }
-
-    if n < 9 {
-        return true;
-    }
-
-    if n % 3 == 0 {
-        return false;
-    }
-
-    let limit = (n as f64).sqrt() as u32;
-    let mut number = 5;
-
-    while number <= limit {
-        if n % number == 0 {
+        if n <= 1 {
             return false;
         }
 
-        if n % (number + 2) == 0 {
+        if n < 4 {
+            return true;
+        }
+
+        if n % 2 == 0 {
             return false;
         }
 
-        number += 6;
-    }
+        if n < 9 {
+            return true;
+        }
 
-    true
+        if n % 3 == 0 {
+            return false;
+        }
+
+        let limit = (n as f32).sqrt() as u32;
+        let mut number = 5;
+
+        while number <= limit {
+            if self % number == 0 {
+                return false;
+            }
+
+            if self % (number + 2) == 0 {
+                return false;
+            }
+
+            number += 6;
+        }
+
+        true
+    }
 }
 
 fn nth_prime(bound: usize) -> u32 {
@@ -50,7 +58,7 @@ fn nth_prime(bound: usize) -> u32 {
     while count < bound {
         number += 2;
 
-        if is_prime(number) {
+        if number.is_prime() {
             count += 1;
         }
     }
@@ -61,10 +69,10 @@ fn nth_prime(bound: usize) -> u32 {
 pj_euler::run!("10001st prime", nth_prime(10_001));
 
 pj_euler::test!(
-    {one_is_not_prine, is_prime(1), false},
-    {two_is_prime, is_prime(2), true},
-    {three_is_prime, is_prime(3), true},
-    {four_is_not_prime, is_prime(4), false},
+    {one_is_not_prine, 1.is_prime(), false},
+    {two_is_prime, 2.is_prime(), true},
+    {three_is_prime, 3.is_prime(), true},
+    {four_is_not_prime, 4.is_prime(), false},
     {sixth_prime_is_thirteen, nth_prime(6), 13},
     {second_prime_is_three, nth_prime(2), 3}
 );
