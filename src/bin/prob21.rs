dadd29d83f7171a8065053aa9e936d95db3a01c8
CPea <cpea2506@gmail.com>
2023-07-08 23:23:46 +0700

    prob21: do not repeat urself

--- src/bin/prob21.rs
+++ src/bin/prob21.rs
@@ -2,49 +2,25 @@
 
 type Number = usize;
 
-trait Math {
-    fn sqrt(self) -> Self;
-    fn divisors_sum(self) -> Number;
-    fn is_amicable_number(&self) -> bool;
-}
-
-impl Math for Number {
-    fn sqrt(self) -> Self {
-        (self as f64).sqrt() as usize
-    }
-
-    fn divisors_sum(self) -> Number {
-        (2..self.sqrt()).fold(1, |mut acc, v| {
-            if self % v == 0 {
-                acc += v;
-
-                let another_divisor = self / v;
-
-                if another_divisor != self {
-                    acc += another_divisor;
-                }
-            }
-
-            acc
-        })
-    }
-
-    fn is_amicable_number(&self) -> bool {
-        let self_divisors_sum = self.divisors_sum();
-
-        self_divisors_sum != *self && self_divisors_sum.divisors_sum() == *self
-    }
-}
-
 fn sum_amicable_numbers(limit: Number) -> Number {
-    (1..limit).filter(Number::is_amicable_number).sum()
+    let mut divisors_sum = vec![0; limit];
+
+    for i in 1..limit {
+        for d in (i * 2..limit).step_by(i) {
+            divisors_sum[d] += i;
+        }
+    }
+
+    divisors_sum
+        .iter()
+        .enumerate()
+        .filter(|&(a, &b)| a != b && a < b && b < limit && divisors_sum[b] == a)
+        .fold(0, |acc, (a, b)| acc + a + b)
 }
 
 pj_euler::run!("Amicable Numbers", sum_amicable_numbers(10000));
 pj_euler::test!(
     amicable_numbers {
-        {amicable_numbers_include_220, 220.is_amicable_number(), true},
-        {amicable_numbers_include_284, 284.is_amicable_number(), true},
-        {amicable_numbers_do_not_include_2, 2.is_amicable_number(), false}
+        {sum_amicable_numbers_less_than_1000, sum_amicable_numbers(1000), 220 + 284}
     }
 );
